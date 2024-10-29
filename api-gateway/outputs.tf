0b2840d4f8ba385e95a87035348d013fa4890793
Maurice Murphy <mauricetjmurpy@gmail.com>
2024-10-29 14:03:06 +0700

    update api-gateway module

--- api-gateway/outputs.tf
+++ api-gateway/outputs.tf
@@ -1,5 +1,14 @@
+output "api_id" {
+  description = "The API identifier."
+  value       = aws_apigatewayv2_api.this[0].id
+}
+
+output "api_arn" {
+  description = "The API ARN."
+  value       = aws_apigatewayv2_api.this[0].arn
+}
 
 output "api_endpoint" {
-  description = "The base URL of the API Gateway"
+  description = "The base URL of the API Gateway."
   value       = aws_apigatewayv2_api.this[0].api_endpoint
 }
