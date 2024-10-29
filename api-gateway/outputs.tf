2ff16af5b91ee831ee49067df787cbd22958510d
Maurice Murphy <mauricetjmurpy@gmail.com>
2024-10-29 13:40:26 +0700

    update api-gateway module

--- api-gateway/outputs.tf
+++ api-gateway/outputs.tf
@@ -1,4 +1,5 @@
+
 output "api_url" {
   description = "The base URL of the API Gateway"
-  value       = "${aws_apigatewayv2_api.this.invoke_url}"
+  value       = "${aws_apigatewayv2_api.this[count.index].api_endpoint}" 
 }
