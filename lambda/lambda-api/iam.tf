77107dde867131df44d5e836c2841ec7f2572a45
Maurice Murphy <mauricetjmurpy@gmail.com>
2024-10-30 15:40:06 +0700

    update lambda module

--- lambda/lambda-api/iam.tf
+++ lambda/lambda-api/iam.tf
@@ -1,6 +1,14 @@
 ##----------------------------------------------------------------------------- 
 ## Below resource will deploy IAM role in AWS environment.   
 ##-----------------------------------------------------------------------------
+resource "aws_lambda_permission" "allow_apigateway_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.main.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:<api-id>/*/*/*"
+}
+
 resource "aws_iam_role" "lambda_exec" {
   name               = "${var.lambda_function_name}-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
