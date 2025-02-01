b25a02ccee90824fa6de4befc64337bf82d1ec35
Maurice Murphy <mauricetjmurpy@gmail.com>
2025-02-01 12:19:01 +0000

    update lambda module

--- lambda/lambda-api/variables.tf
+++ lambda/lambda-api/variables.tf
@@ -1,13 +1,7 @@
-variable "region" {
-  type        = string
-  default     = "us-east-1"
-  description = "AWS region"
-}
-
 variable "name" {
   type        = string
   default     = ""
-  description = "Name  (e.g. `app` or `cluster`)."
+  description = "Name (e.g. `app` or `cluster`)."
 }
 
 variable "s3_bucket" {
@@ -28,38 +22,13 @@ variable "use_s3" {
 
 variable "lambda_exec_role_arn" {
   type        = string
-  default     = ""
-  description = "Lambda execution iam role"
-}
-
-variable "repository" {
-  type        = string
-  default     = "https://github.com/mauricetjmurphy/terraform-modules/tree/master/lambda"
-  description = "Terraform current module repo"
+  description = "IAM execution role ARN for the Lambda function."
 }
 
 variable "environment" {
   type        = string
   default     = ""
   description = "Environment (e.g. `prod`, `dev`, `staging`)."
-}
-
-variable "label_order" {
-  type        = list(any)
-  default     = ["name", "environment"]
-  description = "Label order, e.g. `name`,`application`."
-}
-
-variable "attributes" {
-  type        = list(any)
-  default     = []
-  description = "Additional attributes (e.g. `1`)."
-}
-
-variable "managedby" {
-  type        = string
-  default     = "mauricetjmurphy@gmail.com"
-  description = "ManagedBy, eg 'Gemtech Solutions'."
 }
 
 variable "description" {
@@ -112,19 +81,4 @@ variable "statement_ids" {
   type        = list(string)
   description = "List of statement IDs for Lambda permissions."
   default     = []
-}
-
-variable "iam_actions" {
-  type        = list(string)
-  description = "IAM actions to attach to the Lambda execution role"
-  default     = [
-    "logs:CreateLogStream",
-    "logs:CreateLogGroup",
-    "logs:PutLogEvents"
-  ]
-}
-
-variable "lambda_exec_role_arn" {
-  type        = string
-  description = "IAM execution role ARN for the Lambda function."
 }
