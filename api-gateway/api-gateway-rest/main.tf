2f367e20d3be494e53fea3b3784cb5cc6dd88704
Maurice Murphy <mauricetjmurpy@gmail.com>
2025-02-05 05:23:10 +0000

    update rest api gateway module

--- api-gateway/api-gateway-rest/main.tf
+++ api-gateway/api-gateway-rest/main.tf
@@ -2,9 +2,140 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_api_gateway_rest_api" "this" {
+##----------------------------------------------------------------------------------
+## Labels module for Naming and Tags
+##----------------------------------------------------------------------------------
+module "labels" {
+  source      = "../../labels"
   name        = var.api_name
-  description = var.api_description
+  environment = var.environment
+  label_order = var.label_order
+  repository  = var.repository
 }
 
+##----------------------------------------------------------------------------------
+## REST API Gateway
+##----------------------------------------------------------------------------------
+resource "aws_api_gateway_rest_api" "rest_api" {
+  name        = module.labels.id
+  description = var.api_description
+  tags        = module.labels.tags
 
+  endpoint_configuration {
+    types = [var.endpoint_configuration]
+  }
+}
+
+##----------------------------------------------------------------------------------
+## Resource Policy
+##----------------------------------------------------------------------------------
+resource "aws_api_gateway_rest_api_policy" "public_api_policy" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": "execute-api:Invoke",
+            "Resource": "${aws_api_gateway_rest_api.rest_api.execution_arn}/*"
+        }
+    ]
+}
+EOF
+}
+
+##----------------------------------------------------------------------------------
+## Deployment & Stage
+##----------------------------------------------------------------------------------
+resource "aws_api_gateway_deployment" "api_deployment" {
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  triggers = {
+    redeployment = timestamp()
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  stage_name    = var.stage_name
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  deployment_id = aws_api_gateway_deployment.api_deployment.id
+}
+
+##----------------------------------------------------------------------------------
+## Create API Resources Dynamically
+##----------------------------------------------------------------------------------
+resource "aws_api_gateway_resource" "api_resources" {
+  for_each    = var.api_resources
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  path_part   = each.value.path_part
+}
+
+##----------------------------------------------------------------------------------
+## Create API Methods for Each Resource
+##----------------------------------------------------------------------------------
+resource "aws_api_gateway_method" "rest_api_method" {
+  for_each      = var.api_resources
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.api_resources[each.key].id
+  http_method   = each.value.http_method
+  authorization = var.authorization
+}
+
+##----------------------------------------------------------------------------------
+## Create API Gateway Integrations for Each Method
+##----------------------------------------------------------------------------------
+resource "aws_api_gateway_integration" "rest_api_integration" {
+  for_each                = var.api_resources
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.api_resources[each.key].id
+  http_method             = aws_api_gateway_method.rest_api_method[each.key].http_method
+  integration_http_method = var.http_method
+  # connection_type         = var.connection_rest_api_type
+  # connection_id           = var.connection_id
+  # credentials             = var.credentials
+  # request_templates       = var.request_templates
+  # request_parameters      = var.request_parameters
+  # cache_namespace         = var.cache_namespace
+  # content_handling        = var.content_handling
+  # cache_key_parameters    = var.cache_key_parameters
+  type                    = var.gateway_integration_type
+  timeout_milliseconds    = var.timeout_milliseconds
+  uri                     = each.value.integration_uri
+}
+
+##----------------------------------------------------------------------------------
+## API Gateway Method Response
+##----------------------------------------------------------------------------------
+resource "aws_api_gateway_method_response" "rest_api_method_response" {
+  for_each          = var.api_resources
+  rest_api_id       = aws_api_gateway_rest_api.rest_api.id
+  resource_id       = aws_api_gateway_resource.api_resources[each.key].id
+  http_method       = aws_api_gateway_method.rest_api_method[each.key].http_method
+  status_code       = each.value.status_code
+  response_models   = each.value.response_models
+  response_parameters = each.value.response_parameters
+}
+
+##----------------------------------------------------------------------------------
+## API Gateway Integration Response
+##----------------------------------------------------------------------------------
+resource "aws_api_gateway_integration_response" "rest_api_integration_response" {
+  for_each             = var.api_resources
+  rest_api_id          = aws_api_gateway_rest_api.rest_api.id
+  resource_id          = aws_api_gateway_resource.api_resources[each.key].id
+  http_method          = aws_api_gateway_method.rest_api_method[each.key].http_method
+  status_code          = aws_api_gateway_method_response.rest_api_method_response[each.key].status_code
+  # content_handling     = var.content_handling
+  # response_parameters  = var.integration_response_parameters
+
+  depends_on = [
+    aws_api_gateway_method.rest_api_method,
+    aws_api_gateway_integration.rest_api_integration
+  ]
+}
--- api-gateway/api-gateway-rest/variables.tf
+++ api-gateway/api-gateway-rest/variables.tf
@@ -1,7 +1,12 @@
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
+
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "api_name" {
@@ -19,4 +24,58 @@ variable "stage_name" {
   description = "The name of the stage for deployment"
   type        = string
   default     = "dev"
+}
+
+variable "label_order" {
+  type        = list(any)
+  default     = ["name", "environment"]
+  description = "Label order, e.g. `name`,`application`."
+}
+
+variable "repository" {
+  type        = string
+  default     = ""
+  description = "Terraform current module repo"
+}
+
+variable "endpoint_configuration" {
+  type        = string
+  default     = "EDGE"
+  description = "Endpoint configuration. (Options: REGIONAL or EDGE)"
+}
+
+variable "rest_variables" {
+  type        = map(string)
+  default     = {}
+  description = "Map to set on the stage managed by the stage_name argument."
+}
+
+variable "api_resources" {
+  type        = map(map(string))
+  default     = {}
+  description = "flag to control of resources path"
+}
+
+variable "authorization" {
+  type        = string
+  default     = "NONE"
+  description = "  Required The type of authorization used for the method (NONE, CUSTOM, AWS_IAM, COGNITO_USER_POOLS)"
+}
+
+variable "http_method" {
+  type        = string
+  default     = "ANY"
+  description = "HTTP method (GET, POST, PUT, DELETE, HEAD, OPTION, ANY) when calling the associated resource."
+}
+
+variable "timeout_milliseconds" {
+  type        = number
+  default     = null
+  description = "Custom timeout between 50 and 29,000 milliseconds. The default value is 29,000 milliseconds."
+}
+
+variable "gateway_integration_type" {
+  type        = string
+  default     = "AWS_PROXY"
+  description = "flag tp control the gatway integration type."
 }
