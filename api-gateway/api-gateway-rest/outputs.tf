8e6a59cc9bd8335f06718ff19e3516e13bf075d9
Maurice Murphy <mauricetjmurpy@gmail.com>
2025-02-05 06:31:03 +0000

    update rest api gateway module

--- api-gateway/api-gateway-rest/outputs.tf
+++ api-gateway/api-gateway-rest/outputs.tf
@@ -5,17 +5,17 @@ output "api_url" {
 
 output "api_id" {
   description = "The ID of the API Gateway"
-  value       = aws_api_gateway_rest_api.this.id
+  value       = aws_api_gateway_rest_api.rest_api.id
 }
 
 output "id" {
-  value = aws_api_gateway_rest_api.this.id
+  value = aws_api_gateway_rest_api.rest_api.id
 }
 
 output "root_resource_id" {
-  value = aws_api_gateway_rest_api.this.root_resource_id
+  value = aws_api_gateway_rest_api.rest_api.root_resource_id
 }
 
 output "execution_arn" {
-  value = aws_api_gateway_rest_api.this.execution_arn
+  value = aws_api_gateway_rest_api.rest_api.execution_arn
 }
