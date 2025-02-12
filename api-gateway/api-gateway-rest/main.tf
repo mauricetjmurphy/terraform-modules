f1498f423b080adfd6c63b89142517d0ccda4ebb
Maurice Murphy <mauricetjmurpy@gmail.com>
2025-02-12 06:43:05 +0000

    update api-gateway module

--- api-gateway/api-gateway-rest/main.tf
+++ api-gateway/api-gateway-rest/main.tf
@@ -219,5 +219,42 @@ resource "aws_lambda_permission" "api_gateway_lambda_permission" {
   source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*"
 }
 
+##----------------------------------------------------------------------------------
+## Proxy resources
+##----------------------------------------------------------------------------------
+resource "aws_api_gateway_resource" "proxy_resources" {
+  for_each    = var.api_resources
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  parent_id   = aws_api_gateway_resource.api_resources[each.key].id
+  path_part   = "{proxy+}"
+}
+
+ resource "aws_api_gateway_method" "proxy_methods" {
+  for_each      = var.api_resources
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.proxy_resources[each.key].id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "proxy_integration" {
+  for_each                = var.api_resources
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.proxy_resources[each.key].id
+  http_method             = aws_api_gateway_method.proxy_methods[each.key].http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = each.value.integration_uri
+}
+
+resource "aws_lambda_permission" "api_gateway_lambda_permission" {
+  for_each      = var.api_resources
+  statement_id  = "AllowAPIGatewayInvoke-${each.key}"
+  action        = "lambda:InvokeFunction"
+  function_name = each.value.lambda_arn
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*"
+}
 
 
