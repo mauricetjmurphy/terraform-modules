0720df84eae0e9a2b28ffa39c1c17fd4a8bb9eb0
Maurice Murphy <mauricetjmurpy@gmail.com>
2025-02-12 06:23:57 +0000

    update api-gateway module

--- api-gateway/api-gateway-rest/main.tf
+++ api-gateway/api-gateway-rest/main.tf
@@ -180,11 +180,20 @@ resource "aws_cloudwatch_log_group" "api_gateway_access_logs" {
   retention_in_days = 7
 }
 
-# Create API Gateway methods for each resource
+# Define API Gateway Resources
+resource "aws_api_gateway_resource" "api_resources" {
+  for_each    = var.api_resources
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
+  path_part   = each.value.path_part
+}
+
+# Define API Gateway Methods
 resource "aws_api_gateway_method" "rest_api_method" {
   for_each      = var.api_resources
-  rest_api_id   = module.api_gateway.rest_api_id
-  resource_id   = module.api_gateway.api_resources[each.key]
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.api_resources[each.key].id
   http_method   = each.value.http_method
   authorization = "NONE"
 }
+
