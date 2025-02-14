3f685058a6238a4185305df5b44e6910823f8de0
Maurice Murphy <mauricetjmurpy@gmail.com>
2025-02-14 03:19:50 +0000

    update rds module

--- rds/data.tf
+++ rds/data.tf
@@ -1,10 +1,3 @@
 data "aws_vpc" "specific_vpc" {
   id = "vpc-0a957f3c148b30050"
 }
-
-data "aws_security_group" "lambda_sg" {
-  filter {
-    name   = "group-name"
-    values = ["lambda-security-group"]
-  }
-}
