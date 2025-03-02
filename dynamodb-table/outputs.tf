225ac114727488ce795f8ab3da28aeb551e61368
Maurice Murphy <mauricetjmurpy@gmail.com>
2025-03-02 05:21:18 +0000

    modify the dynamo modules

--- dynamodb-table/outputs.tf
+++ dynamodb-table/outputs.tf
@@ -1,19 +1,35 @@
 output "dynamodb_table_arn" {
   description = "ARN of the DynamoDB table"
-  value       = try(aws_dynamodb_table.this[0].arn, aws_dynamodb_table.autoscaled[0].arn, aws_dynamodb_table.autoscaled_gsi_ignore[0].arn, "")
+  value       = coalesce(
+    length(aws_dynamodb_table.this) > 0 ? aws_dynamodb_table.this[0].arn : null,
+    length(aws_dynamodb_table.autoscaled) > 0 ? aws_dynamodb_table.autoscaled[0].arn : null,
+    length(aws_dynamodb_table.autoscaled_gsi_ignore) > 0 ? aws_dynamodb_table.autoscaled_gsi_ignore[0].arn : null
+  )
 }
 
 output "dynamodb_table_id" {
   description = "ID of the DynamoDB table"
-  value       = try(aws_dynamodb_table.this[0].id, aws_dynamodb_table.autoscaled[0].id, aws_dynamodb_table.autoscaled_gsi_ignore[0].id, "")
+  value       = coalesce(
+    length(aws_dynamodb_table.this) > 0 ? aws_dynamodb_table.this[0].id : null,
+    length(aws_dynamodb_table.autoscaled) > 0 ? aws_dynamodb_table.autoscaled[0].id : null,
+    length(aws_dynamodb_table.autoscaled_gsi_ignore) > 0 ? aws_dynamodb_table.autoscaled_gsi_ignore[0].id : null
+  )
 }
 
 output "dynamodb_table_stream_arn" {
   description = "The ARN of the Table Stream. Only available when var.stream_enabled is true"
-  value       = var.stream_enabled ? try(aws_dynamodb_table.this[0].stream_arn, aws_dynamodb_table.autoscaled[0].stream_arn, aws_dynamodb_table.autoscaled_gsi_ignore[0].stream_arn, "") : null
+  value       = var.stream_enabled ? coalesce(
+    length(aws_dynamodb_table.this) > 0 ? aws_dynamodb_table.this[0].stream_arn : null,
+    length(aws_dynamodb_table.autoscaled) > 0 ? aws_dynamodb_table.autoscaled[0].stream_arn : null,
+    length(aws_dynamodb_table.autoscaled_gsi_ignore) > 0 ? aws_dynamodb_table.autoscaled_gsi_ignore[0].stream_arn : null
+  ) : null
 }
 
 output "dynamodb_table_stream_label" {
   description = "A timestamp, in ISO 8601 format of the Table Stream. Only available when var.stream_enabled is true"
-  value       = var.stream_enabled ? try(aws_dynamodb_table.this[0].stream_label, aws_dynamodb_table.autoscaled[0].stream_label, aws_dynamodb_table.autoscaled_gsi_ignore[0].stream_label, "") : null
+  value       = var.stream_enabled ? coalesce(
+    length(aws_dynamodb_table.this) > 0 ? aws_dynamodb_table.this[0].stream_label : null,
+    length(aws_dynamodb_table.autoscaled) > 0 ? aws_dynamodb_table.autoscaled[0].stream_label : null,
+    length(aws_dynamodb_table.autoscaled_gsi_ignore) > 0 ? aws_dynamodb_table.autoscaled_gsi_ignore[0].stream_label : null
+  ) : null
 }
--- dynamodb-table/variables.tf
+++ dynamodb-table/variables.tf
@@ -1,5 +1,5 @@
 variable "create_table" {
-  description = "Controls if DynamoDB table and associated resources are created"
+  description = "Controls if the DynamoDB table is created"
   type        = bool
   default     = true
 }
@@ -7,113 +7,124 @@ variable "create_table" {
 variable "name" {
   description = "Name of the DynamoDB table"
   type        = string
-  default     = null
 }
 
 variable "attributes" {
-  description = "List of nested attribute definitions. Only required for hash_key and range_key attributes. Each attribute has two properties: name - (Required) The name of the attribute, type - (Required) Attribute type, which must be a scalar type: S, N, or B for (S)tring, (N)umber or (B)inary data"
+  description = "List of attribute definitions (name, type). Required for hash_key and range_key."
   type        = list(map(string))
-  default     = []
 }
 
 variable "hash_key" {
-  description = "The attribute to use as the hash (partition) key. Must also be defined as an attribute"
+  description = "The attribute to use as the partition key (hash key)"
   type        = string
-  default     = null
 }
 
 variable "range_key" {
-  description = "The attribute to use as the range (sort) key. Must also be defined as an attribute"
+  description = "The attribute to use as the sort key (optional)"
   type        = string
   default     = null
 }
 
 variable "billing_mode" {
-  description = "Controls how you are billed for read/write throughput and how you manage capacity. The valid values are PROVISIONED or PAY_PER_REQUEST"
+  description = "Billing mode: PROVISIONED or PAY_PER_REQUEST"
   type        = string
   default     = "PAY_PER_REQUEST"
 }
 
-variable "write_capacity" {
-  description = "The number of write units for this table. If the billing_mode is PROVISIONED, this field should be greater than 0"
-  type        = number
-  default     = null
-}
-
 variable "read_capacity" {
-  description = "The number of read units for this table. If the billing_mode is PROVISIONED, this field should be greater than 0"
+  description = "Read capacity (only if billing_mode is PROVISIONED)"
   type        = number
   default     = null
 }
 
-variable "point_in_time_recovery_enabled" {
-  description = "Whether to enable point-in-time recovery"
-  type        = bool
-  default     = false
-}
-
-variable "ttl_enabled" {
-  description = "Indicates whether ttl is enabled"
-  type        = bool
-  default     = false
-}
-
-variable "ttl_attribute_name" {
-  description = "The name of the table attribute to store the TTL timestamp in"
-  type        = string
-  default     = ""
+variable "write_capacity" {
+  description = "Write capacity (only if billing_mode is PROVISIONED)"
+  type        = number
+  default     = null
 }
 
 variable "global_secondary_indexes" {
-  description = "Describe a GSI for the table; subject to the normal limits on the number of GSIs, projected attributes, etc."
-  type        = any
+  description = "List of global secondary indexes (GSIs) for the table"
+  type        = list(object({
+    name            = string
+    hash_key        = string
+    range_key       = optional(string)
+    projection_type = string
+  }))
   default     = []
 }
 
 variable "local_secondary_indexes" {
-  description = "Describe an LSI on the table; these can only be allocated at creation so you cannot change this definition after you have created the resource."
-  type        = any
+  description = "List of local secondary indexes (LSIs) for the table"
+  type        = list(object({
+    name            = string
+    range_key       = string
+    projection_type = string
+  }))
   default     = []
 }
 
 variable "replica_regions" {
-  description = "Region names for creating replicas for a global DynamoDB table."
-  type        = any
+  description = "Regions for global DynamoDB table replication"
+  type        = list(object({
+    region_name            = string
+    kms_key_arn            = optional(string)
+    propagate_tags         = optional(bool)
+    point_in_time_recovery = optional(bool)
+  }))
   default     = []
 }
 
 variable "stream_enabled" {
-  description = "Indicates whether Streams are to be enabled (true) or disabled (false)."
+  description = "Enable DynamoDB Streams"
   type        = bool
   default     = false
 }
 
 variable "stream_view_type" {
-  description = "When an item in the table is modified, StreamViewType determines what information is written to the table's stream. Valid values are KEYS_ONLY, NEW_IMAGE, OLD_IMAGE, NEW_AND_OLD_IMAGES."
+  description = "Type of DynamoDB stream view"
+  type        = string
+  default     = null
+}
+
+variable "point_in_time_recovery_enabled" {
+  description = "Enable point-in-time recovery for backups"
+  type        = bool
+  default     = false
+}
+
+variable "ttl_enabled" {
+  description = "Enable TTL (Time-to-Live) for automatic record deletion"
+  type        = bool
+  default     = false
+}
+
+variable "ttl_attribute_name" {
+  description = "Attribute used for TTL expiration timestamps"
   type        = string
   default     = null
 }
 
 variable "server_side_encryption_enabled" {
-  description = "Whether or not to enable encryption at rest using an AWS managed KMS customer master key (CMK)"
+  description = "Enable encryption at rest"
   type        = bool
   default     = false
 }
 
 variable "server_side_encryption_kms_key_arn" {
-  description = "The ARN of the CMK that should be used for the AWS KMS encryption. This attribute should only be specified if the key is different from the default DynamoDB CMK, alias/aws/dynamodb."
+  description = "KMS key ARN for encryption (optional)"
   type        = string
   default     = null
 }
 
 variable "tags" {
-  description = "A map of tags to add to all resources"
+  description = "Tags to apply to DynamoDB resources"
   type        = map(string)
   default     = {}
 }
 
 variable "timeouts" {
-  description = "Updated Terraform resource management timeouts"
+  description = "Timeout settings for resource creation, update, and deletion"
   type        = map(string)
   default = {
     create = "10m"
@@ -122,90 +133,14 @@ variable "timeouts" {
   }
 }
 
-variable "autoscaling_enabled" {
-  description = "Whether or not to enable autoscaling. See note in README about this setting"
+variable "deletion_protection_enabled" {
+  description = "Enable deletion protection for the table"
   type        = bool
   default     = false
-}
-
-variable "autoscaling_defaults" {
-  description = "A map of default autoscaling settings"
-  type        = map(string)
-  default = {
-    scale_in_cooldown  = 0
-    scale_out_cooldown = 0
-    target_value       = 70
-  }
-}
-
-variable "autoscaling_read" {
-  description = "A map of read autoscaling settings. `max_capacity` is the only required key. See example in examples/autoscaling"
-  type        = map(string)
-  default     = {}
-}
-
-variable "autoscaling_write" {
-  description = "A map of write autoscaling settings. `max_capacity` is the only required key. See example in examples/autoscaling"
-  type        = map(string)
-  default     = {}
-}
-
-variable "autoscaling_indexes" {
-  description = "A map of index autoscaling configurations. See example in examples/autoscaling"
-  type        = map(map(string))
-  default     = {}
 }
 
 variable "table_class" {
   description = "The storage class of the table. Valid values are STANDARD and STANDARD_INFREQUENT_ACCESS"
   type        = string
-  default     = null
-}
-
-variable "deletion_protection_enabled" {
-  description = "Enables deletion protection for table"
-  type        = bool
-  default     = null
-}
-
-variable "import_table" {
-  description = "Configurations for importing s3 data into a new table."
-  type        = any
-  default     = {}
-}
-
-variable "ignore_changes_global_secondary_index" {
-  description = "Whether to ignore changes lifecycle to global secondary indices, useful for provisioned tables with scaling"
-  type        = bool
-  default     = false
-}
-
-variable "on_demand_throughput" {
-  description = "Sets the maximum number of read and write units for the specified on-demand table"
-  type        = any
-  default     = {}
-}
-
-variable "restore_date_time" {
-  description = "Time of the point-in-time recovery point to restore."
-  type        = string
-  default     = null
-}
-
-variable "restore_source_name" {
-  description = "Name of the table to restore. Must match the name of an existing table."
-  type        = string
-  default     = null
-}
-
-variable "restore_source_table_arn" {
-  description = "ARN of the source table to restore. Must be supplied for cross-region restores."
-  type        = string
-  default     = null
-}
-
-variable "restore_to_latest_time" {
-  description = "If set, restores table to the most recent point-in-time recovery point."
-  type        = bool
-  default     = null
+  default     = "STANDARD"
 }
