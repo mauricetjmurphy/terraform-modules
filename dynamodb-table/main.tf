a6b210ab52ddaad07a35bc39379020016dae234b
Maurice Murphy <mauricetjmurpy@gmail.com>
2025-03-02 05:35:44 +0000

    update dynamo module

--- dynamodb-table/main.tf
+++ dynamodb-table/main.tf
@@ -7,18 +7,12 @@ resource "aws_dynamodb_table" "this" {
   range_key                   = var.range_key
   stream_enabled              = var.stream_enabled
   stream_view_type            = var.stream_enabled ? var.stream_view_type : null
-  table_class                 = var.table_class  # ✅ Fixed: Directly using var.table_class
+  table_class                 = var.table_class
   deletion_protection_enabled = var.deletion_protection_enabled
 
-  # ✅ Define read/write capacity ONLY if using PROVISIONED mode
-  dynamic "provisioned_throughput" {
-    for_each = var.billing_mode == "PROVISIONED" ? [1] : []
-
-    content {
-      read_capacity  = var.read_capacity
-      write_capacity = var.write_capacity
-    }
-  }
+  # Define read/write capacity only if using PROVISIONED mode
+  read_capacity  = var.billing_mode == "PROVISIONED" ? var.read_capacity : null
+  write_capacity = var.billing_mode == "PROVISIONED" ? var.write_capacity : null
 
   ttl {
     enabled        = var.ttl_enabled
@@ -31,7 +25,6 @@ resource "aws_dynamodb_table" "this" {
 
   dynamic "attribute" {
     for_each = var.attributes
-
     content {
       name = attribute.value.name
       type = attribute.value.type
@@ -40,7 +33,6 @@ resource "aws_dynamodb_table" "this" {
 
   dynamic "local_secondary_index" {
     for_each = var.local_secondary_indexes
-
     content {
       name               = local_secondary_index.value.name
       range_key          = local_secondary_index.value.range_key
@@ -51,29 +43,21 @@ resource "aws_dynamodb_table" "this" {
 
   dynamic "global_secondary_index" {
     for_each = var.global_secondary_indexes
-
     content {
       name               = global_secondary_index.value.name
       hash_key           = global_secondary_index.value.hash_key
       range_key          = lookup(global_secondary_index.value, "range_key", null)
       projection_type    = global_secondary_index.value.projection_type
       non_key_attributes = lookup(global_secondary_index.value, "non_key_attributes", [])
-
-      # ✅ Define read/write capacity ONLY if using PROVISIONED mode
-      dynamic "provisioned_throughput" {
-        for_each = var.billing_mode == "PROVISIONED" ? [1] : []
-
-        content {
-          read_capacity  = lookup(global_secondary_index.value, "read_capacity", null)
-          write_capacity = lookup(global_secondary_index.value, "write_capacity", null)
-        }
-      }
+      
+      # Define read/write capacity only if using PROVISIONED mode
+      read_capacity  = var.billing_mode == "PROVISIONED" ? lookup(global_secondary_index.value, "read_capacity", null) : null
+      write_capacity = var.billing_mode == "PROVISIONED" ? lookup(global_secondary_index.value, "write_capacity", null) : null
     }
   }
 
   dynamic "replica" {
     for_each = var.replica_regions
-
     content {
       region_name            = replica.value.region_name
       kms_key_arn            = lookup(replica.value, "kms_key_arn", null)
@@ -95,7 +79,7 @@ resource "aws_dynamodb_table" "this" {
     update = lookup(var.timeouts, "update", null)
   }
 
-  # ✅ Prevents unnecessary Terraform modifications to GSIs
+  # Prevents unnecessary Terraform modifications to GSIs
   lifecycle {
     ignore_changes = [global_secondary_index]
   }
