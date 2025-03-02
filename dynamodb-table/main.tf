f151a535eaf5f7b28acc32a9bf3d39978dd8539d
Maurice Murphy <mauricetjmurpy@gmail.com>
2025-03-02 05:41:06 +0000

    update dynamo module

--- dynamodb-table/main.tf
+++ dynamodb-table/main.tf
@@ -10,9 +10,15 @@ resource "aws_dynamodb_table" "this" {
   table_class                 = var.table_class
   deletion_protection_enabled = var.deletion_protection_enabled
 
-  # Define read/write capacity only if using PROVISIONED mode
-  read_capacity  = var.billing_mode == "PROVISIONED" ? var.read_capacity : null
-  write_capacity = var.billing_mode == "PROVISIONED" ? var.write_capacity : null
+  # ✅ **Define read/write capacity only when using `PROVISIONED` mode**
+  dynamic "provisioned_throughput" {
+    for_each = var.billing_mode == "PROVISIONED" ? [1] : []
+
+    content {
+      read_capacity  = var.read_capacity
+      write_capacity = var.write_capacity
+    }
+  }
 
   ttl {
     enabled        = var.ttl_enabled
@@ -49,8 +55,8 @@ resource "aws_dynamodb_table" "this" {
       range_key          = lookup(global_secondary_index.value, "range_key", null)
       projection_type    = global_secondary_index.value.projection_type
       non_key_attributes = lookup(global_secondary_index.value, "non_key_attributes", [])
-      
-      # Define read/write capacity only if using PROVISIONED mode
+
+      # ✅ **Define throughput only when `PROVISIONED` mode is used**
       read_capacity  = var.billing_mode == "PROVISIONED" ? lookup(global_secondary_index.value, "read_capacity", null) : null
       write_capacity = var.billing_mode == "PROVISIONED" ? lookup(global_secondary_index.value, "write_capacity", null) : null
     }
@@ -79,8 +85,8 @@ resource "aws_dynamodb_table" "this" {
     update = lookup(var.timeouts, "update", null)
   }
 
-  # Prevents unnecessary Terraform modifications to GSIs
+  # ✅ Prevents unnecessary Terraform modifications to GSIs
   lifecycle {
-    ignore_changes = [global_secondary_index]
+    ignore_changes = [billing_mode, global_secondary_index]
   }
 }
