fbfbc6c32e23253655696158ca3d20fd41702643
Maurice Murphy <mauricetjmurpy@gmail.com>
2025-03-14 06:33:10 +0000

    update cloudfront module

--- cloudfront/static-website/cfd.tf
+++ cloudfront/static-website/cfd.tf
@@ -32,7 +32,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
         forward = "none"
       }
     }
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
