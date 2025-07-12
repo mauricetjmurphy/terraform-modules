4a9503a7fbce82a0150fef9f3a9bd7bdf1999bd3
Admin <admin@macbookpro.home>
2025-07-12 21:16:36 +0100

    update api-gateway module

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -60,10 +60,34 @@ resource "aws_api_gateway_integration" "integration" {
   uri                     = each.value.config.integration_uri
 }
 
+resource "aws_api_gateway_resource" "payment_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_resource.resource["payment"].id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "payment_proxy_method" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.payment_proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "payment_proxy_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.payment_proxy.id
+  http_method             = aws_api_gateway_method.payment_proxy_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = data.aws_lambda_function.payment_service_lambda.invoke_arn
+}
+
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [
     aws_api_gateway_method.method,
-    aws_api_gateway_integration.integration
+    aws_api_gateway_integration.integration,
+    aws_api_gateway_method.payment_proxy_method,
+    aws_api_gateway_integration.payment_proxy_integration
   ]
 
   rest_api_id = aws_api_gateway_rest_api.this.id
