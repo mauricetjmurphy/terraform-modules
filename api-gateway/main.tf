d327790f9081d614c084dcf1dfae414fd4fadcd8
Admin <admin@macbookpro.home>
2025-07-12 10:09:13 +0100

    update api-gateway module

--- api-gateway/main.tf
+++ api-gateway/main.tf
@@ -12,31 +12,60 @@ resource "aws_api_gateway_resource" "resource" {
 
 resource "aws_api_gateway_method" "method" {
   for_each = {
-    for resource_key, resource in var.api_resources :
-    "${resource_key}" => resource.methods
+    for pair in flatten([
+      for resource_key, resource in var.api_resources :
+      [
+        for method_name, method_config in resource.methods :
+        {
+          key = "${resource_key}-${method_name}"
+          value = {
+            resource_key = resource_key
+            method_name  = method_name
+            config       = method_config
+          }
+        }
+      ]
+    ]) : pair.key => pair.value
   }
 
   rest_api_id   = aws_api_gateway_rest_api.this.id
-  resource_id   = aws_api_gateway_resource.resource[each.key].id
-  http_method   = upper(keys(each.value)[0])
+  resource_id   = aws_api_gateway_resource.resource[each.value.resource_key].id
+  http_method   = upper(each.value.method_name)
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "integration" {
   for_each = {
-    for resource_key, resource in var.api_resources :
-    "${resource_key}" => resource.methods
+    for pair in flatten([
+      for resource_key, resource in var.api_resources :
+      [
+        for method_name, method_config in resource.methods :
+        {
+          key = "${resource_key}-${method_name}"
+          value = {
+            resource_key = resource_key
+            method_name  = method_name
+            config       = method_config
+          }
+        }
+      ]
+    ]) : pair.key => pair.value
   }
 
-  rest_api_id = aws_api_gateway_rest_api.this.id
-  resource_id = aws_api_gateway_resource.resource[each.key].id
-  http_method = upper(keys(each.value)[0])
-  type        = "AWS_PROXY"
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.resource[each.value.resource_key].id
+  http_method             = upper(each.value.method_name)
   integration_http_method = "POST"
-  uri         = each.value[upper(keys(each.value)[0])].integration_uri
+  type                    = "AWS_PROXY"
+  uri                     = each.value.config.integration_uri
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
+  depends_on = [
+    aws_api_gateway_method.method,
+    aws_api_gateway_integration.integration
+  ]
+
   rest_api_id = aws_api_gateway_rest_api.this.id
 
   triggers = {
